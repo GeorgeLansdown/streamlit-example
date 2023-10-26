bc8521a65d998e77b50932da2f5694844e15ac9a
George Lansdown <george.lansdown@mre.medical.canon>
2023-10-26 16:05:30 +0100

    added sleep

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ import pandas as pd
 import streamlit as st
 import requests
 import re
+import time
 
 def filter_output(output):
     return " ".join(re.findall(r"content\":\"([^\"]*)", output))
@@ -21,6 +22,8 @@ if st.button("Upload!"):
 
 
 if st.button("Scientific review"):
+    with st.spinner(text="In progress..."):
+        time.sleep(2)
     st.code("""This paper proposes a semi-supervised meta-learning framework with disentanglement for domain-generalized medical image segmentation, achieving state-of-the-art generalization performance by modeling domain shifts and utilizing unlabeled data.
 
 The problem the paper addresses is the decrease in inference performance on unseen datasets in medical image segmentation due to domain shifts caused by variations in patient populations, scanners, and scan acquisition settings.
@@ -36,20 +39,38 @@ The paper concludes that the proposed semi-supervised meta-learning framework wi
 Future work could focus on exploring additional techniques for disentanglement and further improving the scalability and efficiency of the proposed framework.""", language=None)
 
 
+
+
+
 if st.button("Literature review and related work"):
+    with st.spinner(text="In progress..."):
+        time.sleep(2)
     st.code("""This paper addresses the problem of decreased performance in medical image segmentation when faced with domain shifts, proposing a semi-supervised meta-learning framework with disentanglement that utilizes unlabeled data and achieves state-of-the-art generalization performance. 
 
 The previous methods mentioned in the paper include direct augmentation of source domain data, feature space regularization, alignment of source domain features or output distributions, and learning domain-invariant features with gradient-based meta-learning. These methods have limitations in scalability, efficiency, and accurately approximating true domain shifts in medical image segmentation. 
 
 Related work to this paper includes research on domain generalization, gradient-based meta-learning methods, disentangled representation learning, unsupervised learning, and domain-invariant feature representation. Techniques such as cross-site MRI harmonization and domain adaptation are also mentioned as related work in handling domain shifts in medical imaging. """, language=None)
+
+
+
+
+
 if st.button("Lay summary: "):
+    with st.spinner(text="In progress..."):
+        time.sleep(2)
+    
     st.code("""This paper is about improving the performance of computer models in analyzing medical images. It introduces a new method that helps the models to better understand and handle differences in images from different sources, like different hospitals or scanners. By using this method, the models can give more accurate results and be more useful in medical diagnosis and treatment. 
 
 The major problems this paper tries to solve are the decrease in performance of computer models when analyzing medical images from different sources, and the difficulty of creating enough labeled data for training these models. The paper proposes a solution that addresses these problems by modeling the differences between sources and using unlabeled data to improve the models' performance. 
 
 The key findings of the paper show that the proposed method achieves state-of-the-art generalization performance on benchmark datasets for medical image segmentation. It outperforms existing methods and demonstrates the effectiveness of modeling domain shifts and utilizing unlabeled data. This means that the models can accurately analyze medical images from different sources, even when they haven't been specifically trained on those sources. """, language=None)
 
+
+
+
 if st.button("Extracting table and figures into LaTex text"):
+    with st.spinner(text="In progress..."):
+        time.sleep(2)
     st.code("""\begin{table}[h] 
 \centering 
 \caption{Dice (%) results and the standard deviations on M&Ms dataset. For "SDNet+Aug." and our method, the training data contain all the unlabeled data and 2% or 5% of labeled data from source domains. The other models are trained by 2% or 5% labeled data only. Bold numbers denote the best performance.} 
@@ -65,7 +86,12 @@ if st.button("Extracting table and figures into LaTex text"):
 & & 78.64±±5.8 & 83.77±±5.1 & 71.29±±3.6 & 74.88±±4.6 & 79.75±±4.4 \ \hline 
 \end{tabular} 
 \end{table} """, language=None)
+    
+
+
 if st.button("Extracting table and figures into Word format content"):
+    with st.spinner(text="In progress..."):
+        time.sleep(2)
     st.code("""Table 1. Dice (%) results and the standard deviations on M&Ms dataset. For "SDNet+Aug." and our method, the training data contain all the unlabeled data and 2% or 5% of labeled data from source domains. The other models are trained by 2% or 5% labeled data only. Bold numbers denote the best performance. 
 
 Source Target nnUNet SDNet+Aug. LDDG SAML Ours 
