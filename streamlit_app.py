c436872443107ab6000d7b135ac239349f5ec4ca
George Lansdown <george.lansdown@mre.medical.canon>
2023-10-26 16:43:41 +0100

    smaller

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ if 'first' not in st.session_state:
     st.session_state.first = True
 
 
-st.image("Canon-logo.jpg", width=568)
+st.image("Canon-logo.jpg", width=300)
 
 st.title("""
 Welcome to CanonPDF!
